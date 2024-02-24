d15d8867a678c0b2741cd6fefcd11bc282ef354e
AlokKhandual <alokkhandual45@gmail.com>
2024-02-24 19:24:39 +0530

    using js

---
+++ script.js
@@ -0,0 +1,18 @@
+var con=document.querySelector("#container")
+
+var love=document.querySelector("i")
+
+con.addEventListener("dblclick",function(){
+    love.style.transform= "translate(-50%,-50%) scale(1)"
+    love.style.opacity= "0.8"
+    love.style.color="red"
+    
+   setTimeout(function(){
+    love.style.opacity="0"
+   },1000)
+
+    setTimeout(function(){
+        love.style.transform= "translate(-50%,-50%) scale(0)"
+
+    },2000)
+})
